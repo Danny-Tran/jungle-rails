b3297b71dd545a79b5e6ce037fa893c20e8dc1fa
Danny-Tran <truong_tran@live.com>
2019-06-13 18:10:17 +0000

    Category are now functioning, able to add and delete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,15 @@
 class CategoriesController < ApplicationController
 
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+
+  def index
+    @Categories = Category.all.order(created_at: :desc)
+  end
+
+  
 
 end
