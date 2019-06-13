defa23aa7de9b5242af49c66330c545c7b747bd9
Danny-Tran <truong_tran@live.com>
2019-06-13 20:26:36 +0000

    user register and login working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,16 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            session[:user_id] = user.id
-            redirect_to '/'
+          session[:user_id] = user.id
+          redirect_to '/'
         else
-            redirect_to '/signup'
-    end  
+          redirect_to '/signup'
+        end
+    end
 
     private
         def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-        end
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        
     end
 end
