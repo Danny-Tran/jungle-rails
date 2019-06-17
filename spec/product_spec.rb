f970d988564d8399c02839747387f9cd04c9b8c5
Danny-Tran <truong_tran@live.com>
2019-06-17 21:12:41 +0000

    fail message are now log on each fail message

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -6,30 +6,37 @@ RSpec.describe Product, type: :model do
       
       it 'Category should have a name' do
         @Ncategory = Category.new name:"testing"
-        
         expect(@Ncategory.name).to be_present
+        @Ncategory.errors.full_messages
       end
 
       it 'Product should have a name' do
         @Nproduct = Product.new name:'newProduct'
         expect(@Nproduct.name).to be_present
+        @Nproduct.errors.full_messages
       end
 
       it 'Product should have price' do
         @Nproduct = Product.new price:10.99
         expect(@Nproduct.price).to be_present
+        @Nproduct.errors.full_messages
       end
       
       it 'Product should have a quantity' do
         @Nproduct = Product.new quantity:10
         expect(@Nproduct.quantity).to be_present
+        @Nproduct.errors.full_messages
       end
 
       it 'Product should have a Category' do
         @Ncategory = Category.new name:"testing2"
         @Nproduct = Product.new category:@Ncategory
         expect(@Nproduct.category).to be_present
+        @Ncategory.errors.full_messages
+        @Nproduct.errors.full_messages
       end
+
+
 
     end
 
