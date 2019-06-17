0aa426bed364c97b091b017f45c43f1b6f0e198a
Danny-Tran <truong_tran@live.com>
2019-06-17 20:46:09 +0000

    validation for product and category are working

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,12 +1,34 @@
+require 'rails_helper'
 RSpec.describe Product, type: :model do
 
     describe 'Validations' do
       # validation tests/examples here
       
-      it 'should have a name' do
-        @catergory = Catergory.new
+      it 'Category should have a name' do
+        @Ncategory = Category.new name:"testing"
+        
+        expect(@Ncategory.name).to be_present
+      end
 
-        expect(@catergory.name).should be_true
+      it 'Product should have a name' do
+        @Nproduct = Product.new name:'newProduct'
+        expect(@Nproduct.name).to be_present
+      end
+
+      it 'Product should have price' do
+        @Nproduct = Product.new price:10.99
+        expect(@Nproduct.price).to be_present
+      end
+      
+      it 'Product should have a quantity' do
+        @Nproduct = Product.new quantity:10
+        expect(@Nproduct.quantity).to be_present
+      end
+
+      it 'Product should have a Category' do
+        @Ncategory = Category.new name:"testing2"
+        @Nproduct = Product.new category:@Ncategory
+        expect(@Nproduct.category).to be_present
       end
 
     end
